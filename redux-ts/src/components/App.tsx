40a5aa9718ec0f2aa77df6fa92e225b0dc0146d6
NAGARRO\swatantragoswami <swatantra.goswami_cs17@nagarro.com>
2021-12-30 15:28:02 +0530

    react and typescript updated

---
+++ redux-ts/src/components/App.tsx
@@ -0,0 +1,17 @@
+import React from "react";
+import { Provider } from "react-redux";
+import { store } from "../state/store";
+import RepositoriesList from "./RepositoriesList";
+
+const App = () => {
+  return (
+    <Provider store={store}>
+      <div>
+        <h1>Search for a package</h1>
+        <RepositoriesList />
+      </div>
+    </Provider>
+  );
+};
+
+export default App;
---
+++ redux-ts/src/hooks/useActions.ts
@@ -0,0 +1,14 @@
+import { useDispatch } from "react-redux";
+import { bindActionCreators } from "redux";
+import { actionCreators } from "../state";
+
+export const useActions = () => {
+  const dispatch = useDispatch();
+  return bindActionCreators(actionCreators, dispatch);
+
+  // return (
+  //     <div>
+
+  //     </div>
+  // )
+};
