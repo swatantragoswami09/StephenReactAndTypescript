241ba8d5bd8e760c9065d987106992cec886ed6e
NAGARRO\swatantragoswami <swatantra.goswami_cs17@nagarro.com>
2022-01-01 01:17:34 +0530

    updated

---
+++ jbook/src/components/code-cell.tsx
@@ -0,0 +1,32 @@
+import { useState } from "react";
+import bundle from "../bundler";
+import CodeEditor from "./code-editor";
+import "bulmaswatch/cyborg/bulmaswatch.min.css";
+import Preview from "./preview";
+import Resizable from "./resizable";
+
+const CodeCell = () => {
+  const [code, setCode] = useState("");
+  const [input, setInput] = useState("");
+
+  const onClick = async () => {
+    const output = await bundle(input);
+    setCode(output);
+  };
+
+  return (
+    <Resizable direction="vertical">
+      <div style={{ height: "100%", display: "flex", flexDirection: "row" }}>
+        <Resizable direction="horizontal">
+          <CodeEditor
+            initialValue="const a=1;"
+            onChange={(value) => setInput(value)}
+          />
+        </Resizable>
+        <Preview code={code} />
+      </div>
+    </Resizable>
+  );
+};
+
+export default CodeCell;
