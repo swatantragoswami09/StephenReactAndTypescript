93abdcf4273a127f86d67828040a03bb4adfddc8
NAGARRO\swatantragoswami <swatantra.goswami_cs17@nagarro.com>
2022-01-03 16:25:39 +0530

    updated Reactbook

--- jbook/src/state/action-creators/index.ts
+++ jbook/src/state/action-creators/index.ts
@@ -1,4 +1,5 @@
 import { ActionType } from "../action-types";
+import { Dispatch } from "redux";
 import {
   Action,
   UpdateCellAction,
@@ -6,8 +7,11 @@ import {
   MoveCellAction,
   InsertCellAfterAction,
   Direction,
+  BundleStartAction,
+  BundleCompleteAction,
 } from "../actions";
 import { CellTypes } from "../cell";
+import bundle from "../../bundler";
 
 export const updateCell = (id: string, content: string): UpdateCellAction => {
   return {
@@ -43,5 +47,25 @@ export const insertCellAfter = (
       id,
       type: cellType,
     },
+  };
+};
+
+export const createBundle = (cellId: string, input: string) => {
+  return async (dispatch: Dispatch<Action>) => {
+    dispatch({
+      type: ActionType.BUNDLE_START,
+      payload: {
+        cellId: cellId,
+      },
+    });
+    const result = await bundle(input);
+
+    dispatch({
+      type: ActionType.BUNDLE_COMPLETE,
+      payload: {
+        cellId: cellId,
+        bundle: result,
+      },
+    });
   };
 };
